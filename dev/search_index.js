3c345c8e1245b5bd254d3c236d6ecea7bd72c255
Documenter.jl <documenter@juliadocs.github.io>
2022-01-20 01:17:47 +0000

    build based on d0bc806

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DataGraphs","category":"page"},{"location":"#DataGraphs","page":"Home","title":"DataGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DataGraphs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DataGraphs]","category":"page"}]
+}
