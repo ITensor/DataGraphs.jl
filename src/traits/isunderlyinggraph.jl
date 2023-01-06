14b43d58ff99fd899991b4cd0267a8bf2f7074e9
mtfishman <mfishman@flatironinstitute.org>
2023-01-05 21:37:34 -0500

    Change IsUnderlyingGraph definitions

--- src/traits/isunderlyinggraph.jl
+++ src/traits/isunderlyinggraph.jl
@@ -1,9 +1,9 @@
 @traitdef IsUnderlyingGraph{X}
-@traitimpl IsUnderlyingGraph{AbstractSimpleGraph}
-@traitimpl IsUnderlyingGraph{AbstractNamedGraph}
-## @traitimpl IsUnderlyingGraph{X} <- is_underlying_graph(X)
+#! format: off
+@traitimpl IsUnderlyingGraph{X} <- is_underlying_graph(X)
+#! format: on
 
-## is_underlying_graph(::Type{<:AbstractGraph}) = false
-## 
-## is_underlying_graph(::Type{<:AbstractSimpleGraph}) = true
-## is_underlying_graph(::Type{<:AbstractNamedGraph}) = true
+is_underlying_graph(::Type{<:AbstractGraph}) = false
+
+is_underlying_graph(::Type{<:AbstractSimpleGraph}) = true
+is_underlying_graph(::Type{<:AbstractNamedGraph}) = true
