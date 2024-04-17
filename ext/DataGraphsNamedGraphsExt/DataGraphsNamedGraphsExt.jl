0d6299f954e81fcf3e6d1c4d047e0866d21cfbf8
Matt Fishman <mtfishman@users.noreply.github.com>
2024-04-16 20:35:02 -0400

    Format
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/DataGraphsNamedGraphsExt/DataGraphsNamedGraphsExt.jl
+++ ext/DataGraphsNamedGraphsExt/DataGraphsNamedGraphsExt.jl
@@ -4,10 +4,7 @@ using NamedGraphs: NamedGraphs, AbstractNamedGraph
 
 DataGraphs.is_underlying_graph(::Type{<:AbstractNamedGraph}) = true
 
-for f in [
-  :(NamedGraphs.parent_graph),
-  :(NamedGraphs.parent_vertices_to_vertices),
-]
+for f in [:(NamedGraphs.parent_graph), :(NamedGraphs.parent_vertices_to_vertices)]
   @eval begin
     function $f(graph::AbstractDataGraph, args...; kwargs...)
       return $f(underlying_graph(graph), args...; kwargs...)
