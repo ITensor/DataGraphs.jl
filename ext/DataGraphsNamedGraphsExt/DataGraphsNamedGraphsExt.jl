e58b00d5a0923c897d2d89b0b282b9db16d3e881
mtfishman <mfishman@flatironinstitute.org>
2024-04-24 17:38:08 -0400

    Update to NamedGraphs 0.6

--- ext/DataGraphsNamedGraphsExt/DataGraphsNamedGraphsExt.jl
+++ ext/DataGraphsNamedGraphsExt/DataGraphsNamedGraphsExt.jl
@@ -4,7 +4,7 @@ using NamedGraphs: NamedGraphs, AbstractNamedGraph
 
 DataGraphs.is_underlying_graph(::Type{<:AbstractNamedGraph}) = true
 
-for f in [:(NamedGraphs.parent_graph), :(NamedGraphs.parent_vertices_to_vertices)]
+for f in [:(NamedGraphs.ordinal_graph), :(NamedGraphs.ordinal_vertex_to_vertex)]
   @eval begin
     function $f(graph::AbstractDataGraph, args...; kwargs...)
       return $f(underlying_graph(graph), args...; kwargs...)
