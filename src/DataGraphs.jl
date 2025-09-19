2b885499b147db21dd33cfd1776d07a881cedd7a
Matt Fishman <mtfishman@users.noreply.github.com>
2025-09-19 17:44:24 -0400

    Use NamedGraphs edge arrangement (#44)

--- src/DataGraphs.jl
+++ src/DataGraphs.jl
@@ -3,7 +3,6 @@ module DataGraphs
 include("utils.jl")
 include("traits/isunderlyinggraph.jl")
 include("abstractdatagraph.jl")
-include("arrange.jl")
 include("datagraph.jl")
 # TODO: Turn into a weak dependency once `GraphsExtensions`
 # is split off from `NamedGraphs`.
