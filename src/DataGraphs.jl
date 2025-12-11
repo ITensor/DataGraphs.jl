598147807984782d1b22b5d98513e86da7ebe978
Jack Dunham <jdunham@flatironinstitute.org>
2025-12-11 15:58:02 -0500

    Improve and reorganise indexing interface

--- src/DataGraphs.jl
+++ src/DataGraphs.jl
@@ -4,6 +4,7 @@ include("utils.jl")
 include("traits/isunderlyinggraph.jl")
 include("dataview.jl")
 include("abstractdatagraph.jl")
+include("indexing.jl")
 include("datagraph.jl")
 
 export AbstractDataGraph, DataGraph
