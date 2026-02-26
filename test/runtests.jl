dbc8ccc12f63c60688b6994b76cdcfdfd6ddc070
Matt Fishman <mtfishman@users.noreply.github.com>
2026-02-26 15:01:46 -0500

    Apply update_template patch (#78)

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,10 @@ const pat = r"(?:--group=)(\w+)"
 arg_id = findfirst(contains(pat), ARGS)
 const GROUP = uppercase(
     if isnothing(arg_id)
-        get(ENV, "GROUP", "ALL")
+        arg = get(ENV, "GROUP", "ALL")
+        # For some reason `ENV["GROUP"]` is set to `""`
+        # when running via GitHub Actions, so handle that case:
+        arg == "" ? "ALL" : arg
     else
         only(match(pat, ARGS[arg_id]).captures)
     end
@@ -16,25 +19,25 @@ const GROUP = uppercase(
 """
 match files of the form `test_*.jl`, but exclude `*setup*.jl`
 """
-function istestfile(fn)
+function istestfile(path)
+    fn = basename(path)
     return endswith(fn, ".jl") && startswith(basename(fn), "test_") &&
         !contains(fn, "setup")
 end
 """
 match files of the form `*.jl`, but exclude `*_notest.jl` and `*setup*.jl`
 """
-function isexamplefile(fn)
+function isexamplefile(path)
+    fn = basename(path)
     return endswith(fn, ".jl") && !endswith(fn, "_notest.jl") && !contains(fn, "setup")
 end
 
 @time begin
     # tests in groups based on folder structure
-    for testgroup in filter(isdir, readdir(@__DIR__))
-        if GROUP == "ALL" || GROUP == uppercase(testgroup)
-            groupdir = joinpath(@__DIR__, testgroup)
-            for file in filter(istestfile, readdir(groupdir))
-                filename = joinpath(groupdir, file)
-                @eval @safetestset $file begin
+    for testgroup in filter(isdir, readdir(@__DIR__; join = true))
+        if GROUP == "ALL" || GROUP == uppercase(basename(testgroup))
+            for filename in filter(istestfile, readdir(testgroup; join = true))
+                @eval @safetestset $(basename(filename)) begin
                     include($filename)
                 end
             end
@@ -42,9 +45,9 @@ end
     end
 
     # single files in top folder
-    for file in filter(istestfile, readdir(@__DIR__))
-        (file == basename(@__FILE__)) && continue # exclude this file to avoid infinite recursion
-        @eval @safetestset $file begin
+    for file in filter(istestfile, readdir(@__DIR__; join = true))
+        (basename(file) == basename(@__FILE__)) && continue # exclude this file to avoid infinite recursion
+        @eval @safetestset $(basename(file)) begin
             include($file)
         end
     end
