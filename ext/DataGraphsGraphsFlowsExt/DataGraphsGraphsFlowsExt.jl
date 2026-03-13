953c8062e4ffc35592fd9332ab32eb81f41d4c90
Jack Dunham <jdunham@flatironinstitute.org>
2026-03-13 12:23:13 -0400

    Fix method ambiguity in `GraphsFlows.mincut`

--- ext/DataGraphsGraphsFlowsExt/DataGraphsGraphsFlowsExt.jl
+++ ext/DataGraphsGraphsFlowsExt/DataGraphsGraphsFlowsExt.jl
@@ -2,7 +2,18 @@ module DataGraphsGraphsFlowsExt
 using DataGraphs: AbstractDataGraph, underlying_graph
 using GraphsFlows: GraphsFlows
 
-function GraphsFlows.mincut(graph::AbstractDataGraph, args...; kwargs...)
-    return GraphsFlows.mincut(underlying_graph(graph), args...; kwargs...)
+function GraphsFlows.mincut(
+        graph::AbstractDataGraph,
+        source_vertex,
+        target_vertex;
+        kwargs...
+    )
+    return GraphsFlows.mincut(
+        underlying_graph(graph),
+        source_vertex,
+        target_vertex;
+        kwargs...
+    )
 end
+
 end
